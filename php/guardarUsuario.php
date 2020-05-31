aaa5f1d6db6867cefe7cd55840f68a4d894d9681
Unknown <jc.arredondomurillo@ugto.mx>
2020-05-31 15:22:39 -0500

    Registro de usuarios fallido

--- php/guardarUsuario.php
+++ php/guardarUsuario.php
@@ -1,23 +1,28 @@
 <?php
 require 'conexion.php';
-session_start();
-/*
-$Nombre = $_POST['Nombre'];
-$ApellidoP = $_POST['ApellidoP'];
-$ApellidoM = $_POST['ApellidoM'];
-$Correo = $_POST['Correo'];
-$Contrasenia = $_POST['Contrasenia'];
-$Tipo = $_POST['Tipo'];
-*/
- 	$sql= "CALL SP_User('hola', 'mi', 'nombre', 'es', 'goku', '3')";        
-//	$sql= "CALL SP_User('$Nombre', '$ApellidoP', '$ApellidoM', '$Correo', '$Contrasenia', '$Tipo')";        
+
+$Name = $_POST['Name'];
+$LastName = $_POST['LastName'];
+$LastName2 = $_POST['LastName2'];
+$Email = $_POST['Email'];
+$Password = $_POST['Password'];
+//$Tipo = $_POST['Tipo'];
+
+ //	$sql= "CALL SP_User('hola', 'mi', 'nombre', 'ese', 'goku', '3')";        
+	$sql= "CALL SP_User('".$Name."', '".$LastName."', '".$LastName2."', '".$Email."', '".$Password."', '1')";        
         $result1 = mysqli_query($conexion, $sql);
-       // $consulta = mysqli_query($conexion, "SELECT * from User");
-        //$i = mysqli_num_rows($consulta);
+       $consulta = mysqli_query($conexion, "SELECT * from User");
+        $i = mysqli_num_rows($consulta);
     if($result1){
-        echo "Correcto";
+echo '<script type="text/javascript">
+    alert("Registrado con exito");
+window.history.back();
+    </script>';
     }
     else{
-        echo "Error";
+
+echo '<script type="text/javascript">
+    alert("Error");
+    </script>';
     }
 ?>
